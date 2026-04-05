ea73163dee9af3ec23a943071cb2b1e402e675c9
Chathura Bamunusinghe <chathura86@gmail.com>
2026-04-05 14:06:02 -0700

    setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# mediator_chat
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/mediator_chat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mediator_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
